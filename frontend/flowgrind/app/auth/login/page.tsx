2663ec827309c075ba7f5c9e772dfc461760909c
Simon Nyblom <simnyb01@easv365.dk>
2026-03-03 14:42:42 +0100

    PATCH: Removed React and installed Nuxt

--- frontend/flowgrind/app/auth/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import LoginForm from "@/app/components/pages/login/LoginForm";
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
--- frontend/flowgrind/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Dashboard from "@/app/components/pages/dashboard/DashBoard";
-
-const DashboardPage: React.FC = () => {
-  return <Dashboard />;
-};
-
-export default DashboardPage;
