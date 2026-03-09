d95e4829bcfbab3f31866f9af784299fff53ef05
Benjamin Vid Gard <bbvgard@gmail.com>
2026-03-09 22:53:56 +0100

    test: master rules test

--- frontend/flowgrind/src/App.vue
+++ frontend/flowgrind/src/App.vue
@@ -10,6 +10,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <!-- Funny comment -->
       </nav>
     </div>
   </header>
